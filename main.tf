dcf78cbee8e7a485cfa515043e567e49e665965a
adpa <140618692+u002398@users.noreply.github.com>
2023-09-04 14:31:37 +0000

    Update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "nginx" {
-  source = "github.com/u002398/terraform-oci-container-instance"
+  source = "github.com/u002398/terraform-oci-container-instance.git?ref=v0.1.0"
 
   compartment_ocid   = var.compartment_ocid
   subnet_id          = var.subnet_id
